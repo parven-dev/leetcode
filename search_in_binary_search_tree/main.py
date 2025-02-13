340c791d030cb5ed46bec655fe4e8cd094f01190
ryan <39787691+parven-dev@users.noreply.github.com>
2025-02-13 16:50:22 +0530

    added leetcode solution

--- search_in_binary_search_tree/main.py
+++ search_in_binary_search_tree/main.py
@@ -1,83 +1,22 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
-         
-         
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
- 
-    def __init__(self):
-        self.root = None
-    
-    def insert(self, value):
-        
-        if self.root is None: 
-            self.root = TreeNode(value)
-            return True
-        
-        temp = self.root
-        
-        while True:
-            if value == temp.val:
-                return False
             
-            if value < temp.val:
-                if temp.left is None:
-                    temp.left = TreeNode(value)
-                    return True
-                
-                temp = temp.left
-                
-            else:
-                if temp.right is None:
-                    temp.right = TreeNode(value)
-                    return True
-                temp = temp.right
-                
-                
-    
-    def inOrderTraversal(self, node, my_list):
-        if not node:
-            return 
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         
-        my_list.append(node.val)
-        self.inOrderTraversal(node.left, my_list)
-        self.inOrderTraversal(node.right, my_list)
-        
-        
-                
-                
-    def searchBST(self, val: int):
-        current = self.root
-        while current is not None:
-            if val == current.val:
-                my_list  = []
-                self.inOrderTraversal(current, my_list)
-                return my_list
-    
-            elif val < current.val:
-                current = current.left
-            else:
-                current = current.right
-                
-        return None
-    
-    
-                
-        
+        if not root:
+            return None
 
+        if root.val == val:
+            return root
 
-root = [4,2,7,1,3]
-val = 2
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
 
-# root = [4,2,7,1,3]
-# val = 5
-s1 = Solution()
-for value in root:
-    s1.insert(value)
-    
-# print(s1.root.left.val)
-print(s1.searchBST(val))
 
