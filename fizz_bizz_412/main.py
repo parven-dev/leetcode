f4dbf9443aec563b41cafe4db7bc1d583a52825e
ryan <39787691+parven-dev@users.noreply.github.com>
2025-02-22 22:45:39 +0530

    Tc O(n), Sc O(1)

--- fizz_bizz_412/main.py
+++ fizz_bizz_412/main.py
@@ -1,21 +1,19 @@
 class Solution:
-    def fizzBuzz(self, n: int) -> list[str]:
-        
+    def fizzBuzz(self, n: int) -> List[str]:
         fizz_buzz_list = []
         for item in range(1, n+1):
-            if item % 3 == 0:
-                fizz_buzz_list.append("Fizz")
-            if item % 5 == 0:
-                fizz_buzz_list.append("Buzz")
-            if item % 5 == 0 and item % 3 == 0:
+            if item % 15 == 0:
                 fizz_buzz_list.append("FizzBuzz")
+            elif item % 3 == 0:
+                fizz_buzz_list.append("Fizz")
+            elif item % 5 == 0:
+                fizz_buzz_list.append("Buzz")
+            
             else:
-                fizz_buzz_list.append(item)
+                fizz_buzz_list.append(str(item))
                 
         return fizz_buzz_list
                 
-                
-    
     
 s1 = Solution()
 n = 15
